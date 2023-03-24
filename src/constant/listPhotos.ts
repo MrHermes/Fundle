4b85ded4bde34119e6cddc5efa3a53878d671d22
Gabbynts <gabriella.natasya2003@gmail.com>
2023-03-24 15:34:01 +0700

    feat: add listPhotosCard

---
+++ src/constant/listPhotos.ts
@@ -0,0 +1,26 @@
+export const listPhotos = [
+  {
+    id: '1',
+    imgUrl: '/images/Lp1.jpg',
+    title: 'Membantu Korban Banjir',
+    desc: 'Donasi untuk korban yang terkena banjir di daerah celung',
+  },
+  {
+    id: '2',
+    imgUrl: '/images/Lp2.jpg',
+    title: 'Membantu Korban Banjir',
+    desc: 'Donasi untuk korban yang terkena banjir di daerah celung',
+  },
+  {
+    id: '3',
+    imgUrl: '/images/Lp3.jpg',
+    title: 'Membantu Korban Banjir',
+    desc: 'Donasi untuk korban yang terkena banjir di daerah celung',
+  },
+  {
+    id: '4',
+    imgUrl: '/images/Lp4.jpg',
+    title: 'Membantu Korban Banjir',
+    desc: 'Donasi untuk korban yang terkena banjir di daerah celung',
+  },
+];
