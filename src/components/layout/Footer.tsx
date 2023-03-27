90ad593d2eeb9908f2f36b716255898b2bb450d8
Gabbynts <gabriella.natasya2003@gmail.com>
2023-03-27 13:24:42 +0700

    fix: footer layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -27,58 +27,60 @@ function Footer() {
           alt='Footer'
           width={20}
           height={20}
-          className='absolute h-full w-full object-cover object-top'
+          className='absolute top-16 h-full w-full object-cover object-top'
         />
       </div>
-      <div className='layout relative top-1/4 bottom-1/4 flex h-max flex-row flex-wrap justify-between py-9 align-middle lg:top-12'>
-        <div>
-          <Typography
-            sizeVariant='h2'
-            colorVariant='secondary'
-            className='font-bold'
-          >
-            Fundle
-          </Typography>
-          <Typography sizeVariant='c1' colorVariant='secondary'>
-            PT Tanda Bakti Nusantara
-          </Typography>
-          <Typography
-            sizeVariant='c2'
-            colorVariant='secondary'
-            className='font-bold lg:mt-5'
-          >
-            Head Office
-          </Typography>
-          <Typography
-            sizeVariant='c3'
-            colorVariant='secondary'
-            className='mb-3'
-          >
-            Foresta Business Loft 5, Unit 26
-            <br />
-            Kel. Lengkong Kulon, Kec. Pagedangan, Kab. Tangerang
-            <br />
-            Banten
-          </Typography>
-        </div>
-        <div className='my-auto mb-6 text-right'>
-          <Typography
-            sizeVariant='h6'
-            colorVariant='secondary'
-            className='font-bold'
-          >
-            Contact us
-          </Typography>
-          <div className='flex flex-row gap-x-5'>
-            {SocialMedia.map(({ link, icon }) => (
-              <Link
-                key={`${link}${icon}`}
-                href={`${link}`}
-                className='text-3xl text-white hover:text-primary-300'
-              >
-                {icon}
-              </Link>
-            ))}
+      <div className='relative top-24'>
+        <div className='layout relative top-1/4 bottom-1/4 flex h-max flex-row flex-wrap justify-between align-middle lg:top-12'>
+          <div>
+            <Typography
+              sizeVariant='h2'
+              colorVariant='secondary'
+              className='font-bold'
+            >
+              Fundle
+            </Typography>
+            <Typography sizeVariant='c1' colorVariant='secondary'>
+              PT Tanda Bakti Nusantara
+            </Typography>
+            <Typography
+              sizeVariant='c2'
+              colorVariant='secondary'
+              className='font-bold lg:mt-5'
+            >
+              Head Office
+            </Typography>
+            <Typography
+              sizeVariant='c3'
+              colorVariant='secondary'
+              className='mb-3'
+            >
+              Foresta Business Loft 5, Unit 26
+              <br />
+              Kel. Lengkong Kulon, Kec. Pagedangan, Kab. Tangerang
+              <br />
+              Banten
+            </Typography>
+          </div>
+          <div className='my-auto mb-6 text-right'>
+            <Typography
+              sizeVariant='h6'
+              colorVariant='secondary'
+              className='font-bold'
+            >
+              Contact us
+            </Typography>
+            <div className='flex flex-row gap-x-5'>
+              {SocialMedia.map(({ link, icon }) => (
+                <Link
+                  key={`${link}${icon}`}
+                  href='${link}'
+                  className='text-3xl text-white hover:text-primary-300'
+                >
+                  {icon}
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
