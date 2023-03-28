a9d4c3385bfc1c62ac2c9f969648938350d4b751
frederick <frederickhidayat2003@gmail.com>
2023-03-29 00:00:55 +0700

    fix: layouting and effect

--- src/pages/detailDonation.page.tsx
+++ src/pages/detailDonation.page.tsx
@@ -18,7 +18,7 @@ function detailDonation(){
     <Layout>
       <Seo />
         <div className="absolute top-0 bg-primary-100 min-h-[96px] w-full lg:min-h-[72px]"></div>
-        <main className="layout mt-14 p-10 rounded-2xl bg-primary-200">
+        <main className="layout mt-14 pt-20 pb-10 px-20 rounded-2xl bg-primary-200">
           <Image 
             src={"/images/dummy-poster.svg"} //Foto_Event
             alt={"Poster Image"}
@@ -27,13 +27,13 @@ function detailDonation(){
             className="static w-full"
           />
           {/* Nama_Event */}
-          <Typography sizeVariant='h4' colorVariant='primary' className="font-bold mt-5">
+          <Typography sizeVariant='h2' colorVariant='primary' className="font-bold my-5 text-wrap">
             Save Malta Children From Hunger
           </Typography>
-          <div className="flex flex-row justify-between">
+          <div className="flex flex-col justify-between gap-y-4 lg:flex-row lg:gap-y-0">
             <div>
               {/* Jumlah_Donasi */}
-              <Typography sizeVariant='h4' colorVariant='tertiary' className="font-bold mt-5">
+              <Typography sizeVariant='h4' colorVariant='tertiary' className="font-bold">
                 Rp. 1000000000
               </Typography>
               {/* Target_Donasi */}
@@ -42,8 +42,8 @@ function detailDonation(){
               </Typography>
             </div>
 
-            <div className="flex flex-row">
-              <div className="flex flex-row justify-center gap-x-5 mx-2 min-w-[10rem] bg-primary-500 shadow-xl rounded-xl font-semibold">
+            <div className="flex flex-row justify-center lg:justify-end">
+              <div className="flex flex-row justify-center gap-x-5 mx-2 min-w-[10rem] bg-primary-500 shadow-xl rounded-xl font-semibold transition duration-500 hover:scale-110">
                 <div className="my-auto text-3xl text-primary-100">
                   <RiHandHeartLine/>
                 </div>
@@ -57,7 +57,7 @@ function detailDonation(){
                   </Typography>
                 </div>
               </div>
-              <div className="flex flex-row justify-center gap-x-5 mx-2 min-w-[10rem] bg-primary-500 shadow-xl rounded-xl font-semibold">
+              <div className="flex flex-row justify-center gap-x-5 mx-2 min-w-[10rem] bg-primary-500 shadow-xl rounded-xl font-semibold transition duration-500 hover:scale-110">
                 <div className="my-auto text-3xl text-primary-100">
                   <BiTimeFive />
                 </div>
@@ -75,16 +75,16 @@ function detailDonation(){
           </div>
 
           {/* Bar progress uang */}
-          <div className="w-full h-2 my-10">
+          <div className="w-full h-2 my-6">
             <ProgressBar progress={data} classProperty="bg-primary-100"/>
           </div>
 
-          <div className="bg-primary-500 p-5 rounded-xl">
+          <div className="bg-primary-500 p-5 rounded-xl transition duration-500 hover:scale-105">
             {/* Deskripsi Donasi */}
-            <Typography sizeVariant='h6' colorVariant='primary' className="font-bold">
+            <Typography sizeVariant='h4' colorVariant='primary' className="font-bold">
               Tentang Donasi
             </Typography>
-            <Typography sizeVariant='c4' colorVariant='primary' className="text-justify">
+            <Typography sizeVariant='c4' colorVariant='primary' className="text-justify mt-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, reprehenderit dolorem repellat fugiat debitis nobis cupiditate adipisci!
               Praesentium dicta rerum at laudantium perferendis, sint iste, aspernatur est, distinctio ab veniam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Sit ratione impedit voluptates ullam quae obcaecati odit, non ab facilis eum delectus temporibus perspiciatis nisi nam eveniet minima, ut corrupti accusamus.
@@ -106,12 +106,12 @@ function detailDonation(){
             </Typography>
           </div>
           
-          <div className="bg-primary-500 p-10 rounded-xl mt-10">
+          <div className="bg-primary-500 p-10 rounded-xl mt-6">
             <Typography sizeVariant='h6' colorVariant='primary' className="font-bold">
               Donasi
             </Typography>
               {listDonatur.map(({ id, name, money }) => (
-                <div key={id} className="bg-primary-200 p-5 my-3 rounded-xl">
+                <div key={id} className="bg-primary-200 p-5 my-3 rounded-xl shadow-xl">
                   <Typography sizeVariant='c1' colorVariant='primary' className="font-bold">
                     {name}
                   </Typography>
@@ -121,6 +121,7 @@ function detailDonation(){
                 </div>
               ))}
           </div>
+
           <ButtonLink
             href='/'
             className='py-4 px-5 mt-10 mx-auto w-max text-2xl font-extrabold hover:bg-secondary-100/60'
