18762fe1db4b9f02a4c3e783c3c51a2174adda4e
Gabbynts <gabriella.natasya2003@gmail.com>
2023-03-31 15:49:08 +0700

    feat: button submit

---
+++ src/components/buttons/ButtonSubmit.tsx
@@ -0,0 +1,24 @@
+import React, { MouseEventHandler } from 'react';
+
+import Typography from '@/components/Typography';
+
+type ButtonProps = {
+  href: string;
+  className?: string;
+  children?: React.ReactNode;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+};
+
+function ButtonSubmit({ children, href, className, onClick }: ButtonProps) {
+  return (
+    <a href={href} onClick={onClick}>
+      <button
+        className={`${className} inline-flex w-full justify-center rounded-md border border-transparent bg-primary-100 hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:ring-offset-2 sm:ml-3 sm:w-auto`}
+      >
+        <Typography colorVariant='secondary'>{children}</Typography>
+      </button>
+    </a>
+  );
+}
+
+export default ButtonSubmit;
