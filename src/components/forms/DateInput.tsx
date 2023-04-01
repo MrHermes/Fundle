8cff9e3ac5a10d8586bd4de71a0736d12ce2011f
Gabbynts <gabriella.natasya2003@gmail.com>
2023-04-02 05:45:57 +0700

    feat: date input component

---
+++ src/components/forms/DateInput.tsx
@@ -0,0 +1,73 @@
+import get from 'lodash.get';
+import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import { Controller, RegisterOptions, useFormContext } from 'react-hook-form';
+import { IconType } from 'react-icons';
+import { HiOutlineCalendar } from 'react-icons/hi';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+type DatePickerProps = {
+  id: string;
+  placeholder?: string;
+  defaultYear?: number;
+  defaultMonth?: number;
+  defaultValue?: string;
+  validation?: RegisterOptions;
+  showError?: boolean;
+  icon?: IconType;
+} & Omit<ReactDatePickerProps, 'onChange'>;
+
+export default function DateInput({
+  id,
+  placeholder,
+  defaultYear,
+  defaultMonth,
+  defaultValue,
+  validation,
+  showError = true,
+  icon: Icon = HiOutlineCalendar,
+  ...rest
+}: DatePickerProps) {
+  const {
+    formState: { errors },
+    control,
+  } = useFormContext();
+  const error = get(errors, id);
+
+  const defaultDate = new Date();
+  if (defaultMonth) defaultDate.setMonth(defaultMonth);
+  if (defaultYear) defaultDate.setFullYear(defaultYear);
+
+  return (
+    <div className='relative'>
+      <Controller
+        control={control}
+        rules={validation}
+        defaultValue={defaultValue}
+        name={id}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <>
+            <div className='relative mt-1'>
+              <ReactDatePicker
+                name={id}
+                onChange={onChange}
+                onBlur={onBlur}
+                selected={value ? new Date(value) : undefined}
+                className='w-full rounded-xl border-none bg-primary-200 py-4 pl-4 text-base'
+                openToDate={value ? new Date(value) : defaultDate}
+                dateFormat='dd/MM/yyyy'
+                placeholderText={placeholder}
+                dropdownMode='select'
+                {...rest}
+              />
+              <Icon className='pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 transform text-lg text-gray-500' />
+              {showError && error && (
+                <p className='text-error'>{error?.message?.toString()}</p>
+              )}
+            </div>
+          </>
+        )}
+      />
+    </div>
+  );
+}
